4d21f651eb9b5b822fb8bb56578bf9991b5134ee
Jose-MRM <joseronni12@gmail.com>
2024-07-03 22:03:27 -0600

    Add files via upload

---
+++ ESTRUCTURA_DE_CONTROL/Bifurcacion.js
@@ -0,0 +1,27 @@
+//Bifurcacion
+
+//if
+if (true){
+    console.log("verdadero");
+}
+
+
+//if-else
+if (false){
+    console.log("verdadero");
+} else {
+    console.log("SPIDERMAN");
+}
+
+// EJEMPLO USANDO VARIABLES
+
+let edad = 21;
+let legal = 18;
+
+if (edad => legal){
+    console.log("es mayor de edad");
+} else {
+    console.log("es menor de edad");
+}
+
+
---
+++ ESTRUCTURA_DE_CONTROL/practica-if-else.js
@@ -0,0 +1,18 @@
+
+
+num = -8
+if (num > 0) {
+  console.log("El número es positivo");
+} else {
+  console.log("El número es negativo");
+}
+
+//else if
+num = 0
+if (num > 0) {
+  console.log("El número es positivo");
+} else if (num < 0) {
+  console.log("El número es negativo");
+} else {
+  console.log("Error: el número debe ser positivo o negativo")
+}
